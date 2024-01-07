0a0badcf8895f98b4767b5deae7198f82f994e93
HBKurubas <hbkurubas@gmail.com>
2024-01-07 19:10:03 +0300

    update

--- 6th_week/6.1.c
+++ 6th_week/6.1.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-int fonksiyon(int x, int *y, int *z)
+int function(int x, int *y, int *z)
 {
+	//multiply x by *y (x = 2, a is not affected)
 	x*=(*y);
+	
+	//update *y with the result of *y squared, multiplied by x, plus *z (y = b = 2*2*2+3 = 11)
 	(*y)=(*y)*(*y)*x+(*z);
+	
+	//update *z with the sum of *y and x, minus *z (z = c = 11+2-3 = 10)
 	(*z)=(*y)+x-(*z);
 }
 
@@ -11,8 +16,8 @@ int main()
 {
 	int a = 1, b = 2, c = 3;
 	
-	fonksiyon(a, &b, &c);
-	printf("a=%d b=%d c=%d", a, b, c);
+	function(a, &b, &c);
+	printf("a=%d b=%d c=%d", a, b, c);	//a=1 b=11 c=10
 	
 	return 0;
 }
